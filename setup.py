0f8996eca9ad69516ca57d7abf6219c5efc24867
Philipp Lang <plang@slb.com>
2018-09-21 09:37:34 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="azblob",
-    version="0.0.4",
+    version="0.0.5",
     author="Philipp Lang",
     packages=["azblob"],
     url=("https://github.com/plang85/azblob"),
